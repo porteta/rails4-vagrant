0cae55929a795efd897d784365fb1eed8a833b62
Tyler Porter <porteta@gmail.com>
2014-02-25 14:10:48 -0500

    Added common gems to gemfile

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,4 +26,7 @@ Vagrant::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+
+  # BetterErrors::Middleware.allow_ip! ENV[10.0.2.2]
+  BetterErrors::Middleware.allow_ip! ENV['TRUSTED_IP'] if ENV['TRUSTED_IP']
 end
